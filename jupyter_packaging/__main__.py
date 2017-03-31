83696e4425e264fe3a6f5eaa063cabb96d1ff524
Vidar Tonaas Fauske <vidartf@gmail.com>
2017-03-31 17:23:50 +0200

    Add a main entry point

---
+++ jupyter_packaging/__main__.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
+"""
+Copies jupyter-packaging's setupbase.py to the specified directory.
+If no directory is given, it uses the current directory.
+"""
+
+import argparse
+import os
+import shutil
+
+
+def check_dir(dirpath):
+    if not os.path.isdir(dirpath):
+        raise argparse.ArgumentTypeError(
+            'Given path is not a directory: %s' % dirpath)
+    return os.path.abspath(dirpath)
+
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument(
+    'destination', type=check_dir, default='.', nargs='?',
+    help="The directory to copy setupbase.py to.",
+    )
+
+args = parser.parse_args()
+
+here = os.path.dirname(__file__)
+source = os.path.join(here, 'setupbase.py')
+destination = args.destination
+
+shutil.copy(source, destination)
