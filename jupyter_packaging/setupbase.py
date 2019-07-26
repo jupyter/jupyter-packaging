a30548a6a1b1091c1423af43e5e98fd5337dd78f
Vidar Tonaas Fauske <vidartf@gmail.com>
2019-07-26 13:29:55 +0100

    Release 0.3.0

--- jupyter_packaging/setupbase.py
+++ jupyter_packaging/setupbase.py
@@ -46,7 +46,7 @@ else:
         return ' '.join(map(pipes.quote, cmd_list))
 
 
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 
 # ---------------------------------------------------------------------------
 # Top Level Variables
