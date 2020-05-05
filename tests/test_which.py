17fa8000200b902a25a5577bea302a68701ce4ec
MeggyCal <MeggyCal@users.noreply.github.com>
2020-05-05 17:01:57 +0200

    which finds python executable

--- tests/test_which.py
+++ tests/test_which.py
@@ -1,6 +1,7 @@
 
+import sys
 from jupyter_packaging.setupbase import which
 
 
-def test_which_finds_python():
-    assert which('python')
+def test_which_finds_python_executable():
+    assert which(sys.executable)
