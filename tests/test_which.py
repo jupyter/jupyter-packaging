6abe3d24df89492efc3839834b1d2a661eaf667d
MeggyCal <MeggyCal@users.noreply.github.com>
2020-05-14 11:17:19 +0200

    do not pass absolute path to which
    it would be no challenge

--- tests/test_which.py
+++ tests/test_which.py
@@ -1,7 +1,8 @@
 
+import os
 import sys
 from jupyter_packaging.setupbase import which
 
 
 def test_which_finds_python_executable():
-    assert which(sys.executable)
+    assert which(os.path.basename(sys.executable))
