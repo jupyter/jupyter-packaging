039fecd2071796055dde26f2ec28227d1c816cf6
Steven Silvester <stslve@amazon.com>
2020-09-11 14:28:06 -0500

    Release 0.7.2

--- jupyter_packaging/setupbase.py
+++ jupyter_packaging/setupbase.py
@@ -48,7 +48,7 @@ else:
         return ' '.join(map(pipes.quote, cmd_list))
 
 
-__version__ = '0.7.1'
+__version__ = '0.7.2'
 
 # ---------------------------------------------------------------------------
 # Top Level Variables
