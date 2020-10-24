d42e93b201cb6df29c58b3bc53790aa28df26573
Doug Blank <doug.blank@gmail.com>
2020-10-24 13:21:27 -0700

    Update setupbase.py
    Sometimes install_base just isn't set for some reason

--- jupyter_packaging/setupbase.py
+++ jupyter_packaging/setupbase.py
@@ -518,10 +518,11 @@ def _get_develop_handler():
         def install_for_development(self):
             super(_develop, self).install_for_development()
             self.run_command('handle_files')
+            prefix = self.install_base or sys.prefix
             for target_dir, filepaths in self.distribution.data_files:
                 for filepath in filepaths:
                     filename = os.path.basename(filepath)
-                    target = os.path.join(self.install_base, target_dir, filename)
+                    target = os.path.join(prefix, target_dir, filename)
                     self.mkpath(os.path.dirname(target))
                     outf, copied = self.copy_file(filepath, target)
 
