6863f92c2a7e1eeebd5212a789e6210e0e1efcc7
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2020-12-18 14:56:41 +0100

    Release 0.7.8

--- jupyter_packaging/setupbase.py
+++ jupyter_packaging/setupbase.py
@@ -49,7 +49,7 @@ else:
         return ' '.join(map(pipes.quote, cmd_list))
 
 
-__version__ = '0.7.7'
+__version__ = '0.7.8'
 
 # ---------------------------------------------------------------------------
 # Top Level Variables
