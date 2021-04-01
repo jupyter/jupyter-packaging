7427c1facb019028af192f54b2603655fca0f933
Steven Silvester <steven.silvester@ieee.org>
2021-04-01 06:42:10 -0500

    Release 0.8.2

--- jupyter_packaging/setupbase.py
+++ jupyter_packaging/setupbase.py
@@ -56,7 +56,7 @@ else:
         return ' '.join(map(pipes.quote, cmd_list))
 
 
-__version__ = '0.8.1'
+__version__ = '0.8.2'
 
 # ---------------------------------------------------------------------------
 # Top Level Variables
