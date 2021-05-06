3a8dc3c03ac83fdd794b8af10150fe0f3944b9ad
Steven Silvester <steven.silvester@ieee.org>
2021-05-06 06:01:14 -0500

    Release 0.10.1

--- jupyter_packaging/setupbase.py
+++ jupyter_packaging/setupbase.py
@@ -56,7 +56,7 @@ else:
         return ' '.join(map(pipes.quote, cmd_list))
 
 
-__version__ = '0.10.0'
+__version__ = '0.10.1'
 
 # ---------------------------------------------------------------------------
 # Top Level Variables
