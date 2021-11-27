d633efdb8a110844a618de90b5aa1ae63cf4d2e3
Steven Silvester <steven.silvester@ieee.org>
2021-11-27 08:33:09 -0600

    fix docstring

--- jupyter_packaging/setupbase.py
+++ jupyter_packaging/setupbase.py
@@ -429,7 +429,7 @@ def get_version_info(version_str):
     __version__ = '1.4.0.dev0'
 
     # Build up version_info tuple for backwards compatibility
-    pattern = r'(?P<major>\d+).(?P<minor>\d+).(?P<patch>\d+)(?P<rest>.*)'
+    pattern = r'(?P<major>/d+).(?P<minor>/d+).(?P<patch>/d+)(?P<rest>.*)'
     match = re.match(pattern, __version__)
     parts = [int(match[part]) for part in ['major', 'minor', 'patch']]
     if match['rest']:
